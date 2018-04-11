437e30a7f3720f4febd3dd8b56632bc210f37973
TheHishem <hishem.moueddene@yahoo.fr>
2018-04-12 01:42:04 +0200

    Player turns handled

--- app.js
+++ app.js
@@ -6,50 +6,50 @@ app.controller("game", function($scope) {
             "emoji": "🐉",
             "life": 10,
             "points": 0,
+            "playing": true,
         },
         {
             "name": "Dinosaur",
             "emoji": "🦖",
             "life": 10,
             "points": 0,
+            "playing": false,
         },
         {
             "name": "Boar",
             "emoji": "🐗",
             "life": 10,
             "points": 0,
+            "playing": false,
         },
         {
             "name": "Gorilla",
             "emoji": "🦍",
             "life": 10,
             "points": 0,
+            "playing": false,
         },
         {
             "name": "Rhinoceros",
             "emoji": "🦏",
             "life": 10,
             "points": 0,
+            "playing": false,
         },
         {
             "name": "Buffalo",
             "emoji": "🐃",
             "life": 10,
             "points": 0,
+            "playing": false,
         },
     ];
     $scope.playerInTokyo = null;
     $scope.currentPlayer = 0;
     $scope.nbPlayers = 6;
 
-    $scope.changePlayerTurn = function() {
-        if ($scope.currentPlayer == $scope.nbPlayers - 1) {
-            $scope.currentPlayer = 0
-        } else {
-            $scope.currentPlayer += 1
-        }
-    }
 })
+
 
 app.controller("left", function($scope) {})
 
@@ -84,12 +84,35 @@ app.controller("bottom", function($scope) {
     };
 
     $scope.lock = function(d) {
-        if (d.locked) {
-            d.locked = false
-        } else {
-            d.locked = true
+        if ($scope.nbRolls !=3) {
+            if (d.locked) {
+                d.locked = false
+            } else {
+                d.locked = true
+            }
         }
+    }
+
+    $scope.changePlayerTurn = function() {
+        $scope.players[$scope.currentPlayer].playing = false
+        console.log($scope.players[$scope.currentPlayer].name)
+        if ($scope.currentPlayer == $scope.nbPlayers - 1) {
+            $scope.currentPlayer = 0
+        } else {
+            $scope.currentPlayer += 1
+        }
+        $scope.players[$scope.currentPlayer].playing = true
+        $scope.dices = [
+            { "face": "1️⃣", "locked": false },
+            { "face": "2️⃣", "locked": false },
+            { "face": "3️⃣", "locked": false },
+            { "face": "👊", "locked": false },
+            { "face": "💵", "locked": false },
+            { "face": "❤️", "locked": false }
+        ];
     }
 
 
 });
+
+//Faudrait faire en sorte que le bouton roll au bout de 3 fois devienne "calculer" ou un truc comme ça, dès que tu cliques dessus ça calcule tes points automatiquement (pour ça faut faire une fonction je peux m'en occuper) et quand tu cliques sur end turn tu passes au joueur suivant qu'en penses tu ?
